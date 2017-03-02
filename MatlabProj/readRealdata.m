f86f88dee29b8d1f73e29547970212ff232cb914
lnianhong <lnianhong@gmail.com>
2017-03-03 00:08:11 +0800

    add balanced 0228part data

--- MatlabProj/readRealdata.m
+++ MatlabProj/readRealdata.m
@@ -4,15 +4,20 @@ data_tran = csvread('transmitter0228.csv',1,0);
 
 dataT = data_tran(:,2);
 dataR = data_receive(:,2);
+indexT = length(dataT);
+indexR = length(dataR);
+
 if sum(dataR)> sum(dataT)
-    [ tmp,index ] = balanceData( dataR,dataT );
+    [ tmp,indexR ] = balanceData( dataR,dataT );
     dataR = tmp;
 else
     if sum(dataT)> sum(dataR)
-        [ tmp,index ] = balanceData( dataT,dataR );
+        [ tmp,indexT ] = balanceData( dataT,dataR );
         dataT = tmp;
     end
 end
+csvwrite('receiver0228part.csv',[data_receive(1:indexR,1),dataR]);
+csvwrite('transmitter0228part.csv',[data_tran(1:indexT,1),dataT]);
 
 
 

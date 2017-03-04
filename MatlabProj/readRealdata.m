53417d278dd3970b13c4ee9fedea704e63cc563d
LiuNianhong <LiuNianhong>
2017-03-05 00:26:50 +0800

    add out info

--- MatlabProj/readRealdata.m
+++ MatlabProj/readRealdata.m
@@ -3,7 +3,7 @@ data_receive = csvread('receiver0228.csv',1,0);
 data_tran = csvread('transimitter0228.csv',1,0);
 
 dataT = data_tran(:,2);
-dataR = data_receive(:,2);
+dataR = data_receive(1:100000,2);
 
 indexT = length(dataT);
 indexR = length(dataR);
@@ -17,8 +17,8 @@ else
         dataT = tmp;
     end
 end
-dlmwrite('receiver0228part.csv',[data_receive(1:indexR,1),dataR],'precision',16);
-dlmwrite('transimitter0228part.csv',[data_tran(1:indexT,1),dataT],'precision',16);
+dlmwrite('receiver0228_10W.csv',[data_receive(1:indexR,1),dataR],'precision',16);
+dlmwrite('transimitter_10W.csv',[data_tran(1:indexT,1),dataT],'precision',16);
 
 
 
--- VSProj/RequireMatch/main.cpp
+++ VSProj/RequireMatch/main.cpp
@@ -1,89 +1,93 @@
-#include <iostream>
-#include <vector>
+#include <iostream>  
+#include <vector>  
+#include <bitset>  
+#include "Exchange_Item.h"
+#include "requirematch.h"
+#include "CSVRow.h"
+#include <cstdlib>
 #include <algorithm>
-#include <string>
+#include <ctime> 
+#include <iterator> 
 
 using namespace std;
 
-//find if the given subset sum exists.
-int findSubsetSum(vector<int>& arr, int given, vector<bool>& included)
-{
-	int cur = 0; // 指向当前值.
-	int sum = 0; // 当前子集合和.
+//int main() {
+//	//{ 2,5,15,8,20 };
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,20 };
+//	//int arr[] = { 0,20 };
+//	vector<int> W(arr, arr + sizeof(arr) / sizeof(int));
+//	vector<bool> included(sizeof(arr) / sizeof(int), false);
+//	vector<bool> includedv2(sizeof(arr) / sizeof(int), false);
+//	subsetSum(W, included, 0, 20, 0);//decrease order
+//	cout << "*********increase order********" << endl;
+//	subsetSum(W, includedv2, 20, W.size()-1);//increase order
+//
+//	system("pause");
+//	return 0;
+//}
 
-	while (cur >= 0)
+int main() {
+	string suffix("part");
+	clock_t start = clock();
+	ExContainer Rers;//contain of receivers
+	ExContainer Ters;//contain of transimiters
+	//typedef vector<Exchange_Item>::iterator ItemItertype;
+	string Rers_file = "receiver" + suffix + ".csv";
+	string Ters_file	= "transimitter" + suffix + ".csv";
+
+	readCSVdata(Rers, Rers_file,1);// initiation
+	readCSVdata(Ters, Ters_file,1);
+	size_t a = dealRepetition(Rers, Ters);//除了为Res_money==0的元素不再有Rers和Ters中不再有Res相等的了。
+	
+	Rers.sort(compItemNoMaxWeight);
+	Ters.sort(compItemNoMaxWeight);
+
+	// the first Item whose res_money >0 
+	difftype Rp0_num = finishedNum(Rers);
+	//Rers.begin()+Rp0_num ponited the first nonezeroItem
+	difftype Tp0_num = finishedNum(Ters);
+
+	ExContainer::size_type Rers_size = Rers.size();
+	ExContainer::size_type Ters_size = Ters.size();
+
+	while (Rp0_num<Rers_size && Tp0_num < Ters_size)
 	{
-		//if current one is not included
-		if (false == included[cur])
+		if (Ters.back().getResMoney() > Rers.back().getResMoney())
 		{
-			//include current one
-			included[cur] = true;
-			sum += arr[cur];
-			//find the given subset sum
-			if (sum == given)
-			{
-				return 1;
-			}
-			else if (sum > given) //exceed the given sum
-			{
-				included[cur] = false;
-				sum -= arr[cur];
-			}
-			cur++;
+			exchangeFunc(Ters, Rers, Rp0_num, Tp0_num);
 		}
-		//backtrace
-		if (cur >= arr.size())
-		{
-			/*
-			** 下面两个循环依次排除匹配不成功的结果中
-			** 包括在结果内以及不包括在结果内的元素
-			** 直到找到下一个包括在结果内的元素
-			** 例如：用1和0表示包括和未包括
-			** 若结果为1110011，则第三个1为所找元素
-			** 将其变为0，但是从第4个0开始遍历。
-			**/
-			while (true == included[cur - 1])
-			{
-				cur--;
-				included[cur] = false;
-				sum -= arr[cur];
-				//backtrace to the head
-				if (cur < 1)
-					return 0;
-			}
-			while (false == included[cur - 1])
-			{
-				cur--;
-				if (cur < 1)
-					return 0;
-			}
-			//change the status of current - 1,not current!
-			included[cur - 1] = false;
-			sum -= arr[cur - 1];
-		}
+		else
+			exchangeFunc(Rers, Ters, Tp0_num, Rp0_num);
+		if (Rp0_num % 1000 == 0)
+			cout << "Rp0_num/Rers_size:\t" << Rp0_num << "/" << Rers_size << endl;
 	}
+	Rers.sort(compOrder);
+	Ters.sort(compOrder);
+	clock_t finish = clock();
+	string result_log = "log_" + suffix + ".txt";
+	double t = double(finish - start) / CLOCKS_PER_SEC;
+	writeLog(result_log, Rers, Ters, t);
+	cout << t << " (s) " << endl;
+	cout << "Sum of exchange times:\t"<<sumExTimes(Rers) << endl;
+	cout << "Maxium exchange times of Rers:\t" << (*maxExTimes(Rers)).getWeight() << endl;
+	cout << "Maxium exchange times of Ters:\t" << (*maxExTimes(Ters)).getWeight() << endl;
+	string Rresult_file = "R_result_list_" + suffix + ".csv";
+	string Tresult_file = "T_result_list_" + suffix + ".csv";
+	writeResult(Rers, Rresult_file);
+	writeResult(Ters, Tresult_file);
+	system("pause");
 	return 0;
 }
 
 
-
-int main()
-{
-	int arr[] = { 2,5,15,8,20 };
-	vector<int> v(arr, arr + sizeof(arr) / sizeof(int));
-	vector<bool> included(sizeof(arr) / sizeof(int), false);
-	int given = 33;//5+8+20
-
-	if (findSubsetSum(v, given, included))
-	{
-		vector<bool>::iterator iterb = included.begin();
-		vector<int>::iterator iteri = v.begin();
-		for (; iterb != included.end(); iterb++, iteri++)
-		{
-			if (*iterb)
-				cout << *iteri << endl;
-		}
-	}
-	system("pause");
-
-}
+//has2sum(Rers.begin()+ Rp0_num, Rers.end(),Ters.back(),it1,it2);
+//cout << "*************Receiver******************" << endl;
+//for (Itertype iter = Rers.begin(); iter != Rers.end(); ++iter)
+//{
+//	(*iter).print();
+//}
+//cout << "*************Transimiter******************" << endl;
+//for (Itertype iter = Ters.begin(); iter != Ters.end(); ++iter)
+//{
+//	(*iter).print();
+//}
